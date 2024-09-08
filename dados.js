d57a14ecf974f32e9ee314c387690d438ae74183
PerrecosRuindadePura <1979p.campos@gmail.com>
2024-09-08 16:34:09 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados =[
+    {
+        titulo : "Neymar Jr",
+        descricao : "Neymar da Silva Santos Júnior (Mogi das Cruzes, 5 de fevereiro de 1992) é um futebolista brasileiro que atua como atacante. Atualmente joga no Al-Hilal, da Arábia Saudita.",
+        link : "https://www.youtube.com/watch?v=wp1ZltRNdJs&pp=ygUMbmV5bWFyIHNraWxz",
+        tags :"brasil festa menino futebol  "
+    
+    },
+    {
+            titulo : "Messi",
+            descricao : "Lionel Andrés Messi Cuccittini (Rosário, 24 de junho de 1987) é um futebolista argentino que atua como atacante. Atualmente, joga pelo Inter Miami e pela Seleção Argentina, onde, atuando como capitão, venceu a Copa do Mundo do Catar de 2022. Ele detém um recorde de oito premiações da Bola de Ouro pela France Football e oito pela FIFA, seis Chuteiras de Ouro da Europa, um recorde de 91 gols oficiais marcados em um ano e, com 45 títulos conquistados, é o jogador com mais títulos oficiais na história do futebol.[9]",
+            link : "https://www.youtube.com/watch?v=b4_PA7GsLwA&pp=ygULbWVzc2kgc2tpbHM%3D",
+            tags :" anao genio craque futebol " 
+    },
+    {
+        titulo : "Cristiano Ronaldo",
+        descricao :  "Cristiano Ronaldo é uma figura inspiradora e um exemplo de dedicação e sucesso no esporte. Um estudo sobre sua carreira pode oferecer insights valiosos sobre o futebol, o marketing esportivo e a psicologia do esporte.",
+        link : "https://www.youtube.com/watch?v=Yq7Yy8H5J-8&pp=ygUUY3Jpc3RpYW5vIHJvbmFsIHNraXM%3D",
+        tags :"robozao robo monstro futebol"
+    }
+    
+]
+    
+
